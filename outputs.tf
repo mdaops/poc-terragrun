3dbe304788fe605e4dd4f37f1bc90bcea8534404
michael abbott <michael@mabbott.dev>
2024-08-02 16:15:46 +0100

    chore: provision action

--- outputs.tf
+++
@@ -1,9 +0,0 @@
-output "project_id" {
-  value       = google_project.project.project_id
-  description = "The ID of the created project"
-}
-
-output "project_number" {
-  value       = google_project.project.number
-  description = "The number of the created project"
-}
