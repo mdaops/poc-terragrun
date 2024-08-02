181007777b8e168a0be99f3bffe740c2dd03fd8e
michael abbott <michael@mabbott.dev>
2024-08-02 16:25:15 +0100

    chore: remove duplicates

--- main.tf
+++ main.tf
@@ -25,51 +25,8 @@ resource "google_project_service" "services" {
     "containerregistry.googleapis.com",
     "cloudbuild.googleapis.com"
   ])
-  project = google_project.project.project_id
-  service = each.key
-
+  project                    = google_project.project.project_id
+  service                    = each.key
   disable_dependent_services = true
 }
 
-variable "project_id" {
-  description = "The ID of the project"
-  type        = string
-}
-
-variable "repository_name" {
-  description = "The name of the repository"
-  type        = string
-}
-
-variable "owner" {
-  description = "The owner of the project"
-  type        = string
-}
-
-variable "region" {
-  description = "The default region for resources"
-  type        = string
-  default     = "us-central1"
-}
-
-variable "folder_id" {
-  description = "The folder ID to create the project in"
-  type        = string
-}
-
-variable "billing_account" {
-  description = "The billing account to associate with the project"
-  type        = string
-}
-
-# outputs.tf
-
-output "project_id" {
-  value       = google_project.project.project_id
-  description = "The ID of the created project"
-}
-
-output "project_number" {
-  value       = google_project.project.number
-  description = "The number of the created project"
-}
